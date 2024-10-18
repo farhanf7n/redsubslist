fc82993e1fd2faa0c62e0315491f24b7bd79eef8
Farhan Ahmed <farhanf7n@gmail.com>
2024-10-19 00:25:41 +0500

    Added files & code for respective stuff

---
+++ src/components/SubItem.jsx
@@ -0,0 +1,70 @@
+export default function SubItem() {
+  return (
+    <>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-start"
+        href="https://www.reddit.com/r/chutyapa/comments/1ea8cal/how_do_i_stay_secure_and_protected_online_in/"
+      >
+        <div className="border bg-card text-card-foreground shadow flex items-start p-4 w-full rounded-lg transition-transform transform hover:scale-105">
+          <div className="flex flex-col items-start w-16">
+            <div className="flex items-center w-full mb-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="16"
+                height="16"
+                color="#ffffff"
+                fill="none"
+              >
+                <path
+                  d="M17.9998 15C17.9998 15 13.5809 9.00001 11.9998 9C10.4187 8.99999 5.99985 15 5.99985 15"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <span className="text-xs font-medium text-white">43</span>
+            </div>
+            <div className="flex items-center w-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="16"
+                height="16"
+                color="#ffffff"
+                fill="none"
+              >
+                <path
+                  d="M8 13.5H16M8 8.5H12"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M6.09881 19C4.7987 18.8721 3.82475 18.4816 3.17157 17.8284C2 16.6569 2 14.7712 2 11V10.5C2 6.72876 2 4.84315 3.17157 3.67157C4.34315 2.5 6.22876 2.5 10 2.5H14C17.7712 2.5 19.6569 2.5 20.8284 3.67157C22 4.84315 22 6.72876 22 10.5V11C22 14.7712 22 16.6569 20.8284 17.8284C19.6569 19 17.7712 19 14 19C13.4395 19.0125 12.9931 19.0551 12.5546 19.155C11.3562 19.4309 10.2465 20.0441 9.14987 20.5789C7.58729 21.3408 6.806 21.7218 6.31569 21.3651C5.37769 20.6665 6.29454 18.5019 6.5 17.5"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                />
+              </svg>
+              <span className="text-xs font-medium text-white">24</span>
+            </div>
+          </div>
+          <div className="flex-1 min-w-0">
+            <h3 className="text-sm font-medium line-clamp-2 mb-1 text-white">
+              How do I stay secure and protected online in Pakistan? A quick
+              guide.
+            </h3>
+            <p className="text-xs text-muted-foreground truncate text-white">
+              Posted by _NineZero_
+            </p>
+          </div>
+        </div>
+      </a>
+    </>
+  );
+}
